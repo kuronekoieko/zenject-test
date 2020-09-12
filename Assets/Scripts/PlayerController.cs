ee12f5461ef7cf2aac1039fa307fa8703c9454aa
家子智成 <>
2020-09-12 19:00:02 +0900

    テスト実行

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class PlayerController : MonoBehaviour
+{
+    [Inject] GameManager gameManager;
+
+    void Start()
+    {
+        gameManager.ShowLog();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
